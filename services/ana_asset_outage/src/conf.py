fb402bde2acf4ce854dfdd32e66bddf933e772a2
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2024-06-26 15:38:49 +0100

    fix: include mongodb config

--- services/ana_asset_outage/src/conf.py
+++ services/ana_asset_outage/src/conf.py
@@ -5,4 +5,5 @@ import pytz
 env_get = os.environ.get
 
 PORT = env_get("PORT", 8000)
+MONGO_URI = env_get("MONGO_URI", "mongodb://localhost:27017")
 TIMEZONE = pytz.timezone(env_get("TIMEZONE", "Europe/London"))
--- services/ana_hospital_schedule/src/conf.py
+++ services/ana_hospital_schedule/src/conf.py
@@ -3,3 +3,4 @@ import os
 env_get = os.environ.get
 
 PORT = env_get("PORT", 8000)
+MONGO_URI = env_get("MONGO_URI", "mongodb://localhost:27017")
